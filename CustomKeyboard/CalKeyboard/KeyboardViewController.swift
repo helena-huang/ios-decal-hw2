6babdc170ae793b46293ee9066b43fe645db95a5
Helena Huang <helena.huang@berkeley.edu>
2015-09-23 19:41:30 -0700

    basic functionality (nextKeyboard is broken though?)

--- CustomKeyboard/CalKeyboard/KeyboardViewController.swift
+++ CustomKeyboard/CalKeyboard/KeyboardViewController.swift
@@ -9,14 +9,31 @@
 import UIKit
 
 class KeyboardViewController: UIInputViewController {
-
+    
     @IBOutlet var nextKeyboardButton: UIButton!
+    @IBOutlet var shiftButton: UIButton!
+    @IBOutlet var spaceButton: UIButton!
+    @IBOutlet var backspaceButton: UIButton!
+    @IBOutlet var returnButton: UIButton!
+    //@IBOutlet var animalButtons: [UIButton]!
+    @IBOutlet var mouseButton: UIButton!
+    @IBOutlet var catButton: UIButton!
+    @IBOutlet var dogButton: UIButton!
+    @IBOutlet var pigButton: UIButton!
+    @IBOutlet var cowButton: UIButton!
+    @IBOutlet var bearButton: UIButton!
+    @IBOutlet var horseButton: UIButton!
+    @IBOutlet var fishButton: UIButton!
+    @IBOutlet var snakeButton: UIButton!
+    @IBOutlet var tigerButton: UIButton!
     
     var keyboardView: UIView!
-
+    var shiftIsPressed: Bool = false;
+    let arrayOfNoiseStrings: [String] = ["squeak", "meow", "woof", "oink", "moo", "go bears", "neigh", "glub", "hsss", "rawr"]
+    
+    
     override func updateViewConstraints() {
         super.updateViewConstraints()
-    
         // Add custom view sizing constraints here
     }
 
@@ -37,15 +54,52 @@ class KeyboardViewController: UIInputViewController {
     override func textDidChange(textInput: UITextInput?) {
         // The app has just changed the document's contents, the document context has been updated.
     }
-
+    
     func loadInterface() {
         let keyboardNib = UINib(nibName: "Keyboard", bundle: nil)
         keyboardView = keyboardNib.instantiateWithOwner(self, options: nil)[0] as! UIView
         keyboardView.frame = view.frame
         view.addSubview(keyboardView)
         view.backgroundColor = keyboardView.backgroundColor
-        nextKeyboardButton.addTarget(self, action: "advanceToNextInputMode", forControlEvents: .TouchUpInside) // advanceToNextInputMode is already defined in template
+        nextKeyboardButton.addTarget(self, action: "advanceToNextInputMode:", forControlEvents: .TouchUpInside) // advanceToNextInputMode is already defined in template
     }
 
+    @IBAction func pressShift(sender: UIButton) {
+        shiftIsPressed = !shiftIsPressed
+        if shiftIsPressed {
+            shiftButton.backgroundColor = UIColor.grayColor()
+        } else {
+            shiftButton.backgroundColor = UIColor.whiteColor()
+        }
+    }
+    
+    @IBAction func hitSpace(sender: UIButton) {
+        (textDocumentProxy as UIKeyInput).insertText(" ")
+        sender.backgroundColor = UIColor.whiteColor()
+    }
+    
+    @IBAction func hitReturn(sender: UIButton) {
+        (textDocumentProxy as UIKeyInput).insertText("\n")
+        sender.backgroundColor = UIColor.whiteColor()
+    }
+    
+    @IBAction func hitBackspace(sender: UIButton) {
+        (textDocumentProxy as UIKeyInput).deleteBackward()
+        sender.backgroundColor = UIColor.whiteColor()
+    }
+    
+    @IBAction func typeAnimalNoise(sender: UIButton) {
+        if shiftIsPressed {
+            (textDocumentProxy as UIKeyInput).insertText(arrayOfNoiseStrings[sender.tag].uppercaseString)
+            pressShift(shiftButton)
+        } else {
+            (textDocumentProxy as UIKeyInput).insertText(arrayOfNoiseStrings[sender.tag])
+        }
+        sender.backgroundColor = UIColor.whiteColor()
+    }
+    
+    @IBAction func changeColorWhenPressed(sender: UIButton) {
+        sender.backgroundColor = UIColor.grayColor()
+    }
 
 }
